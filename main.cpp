d7c89e52265ded13ddefe77d4b16fd39f1b8b2fc
gremoryyx <arispelgremory@gmail.com>
2022-09-26 22:37:26 +0800

    created main program

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//	Ask the compiler to include minimal header files for our program.
+// #define -> Instruction to communicate with the compiler
+#define WIN32_LEAN_AND_MEAN // Only include the skinny(LEAN) and powerful(MEAN) part of Windows.h
+#include <Windows.h>
+#include <d3d9.h>
+
+//	use WinMain if you don't want the console
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	/*
+	   HINSTANCE hInstance - ID Number
+	   HINSTANCE hPrevInstance - previous window
+	   LPSTR lpCmdLine - Long pointer to a command line (ping.exe ->www.yahoo.com<-)
+	   int nShowCmd - window mode (fullscreen, borderless)
+	*/
+
+	CreateMyWindow();
+	CreateMyDX();
+	CreateMyDInput();
+
+	while (WindowIsRunning()) {
+		GetInput();
+		Update(10);
+		Render();
+	}
+	CleanupMyLevel();
+	CleanUpMyDirectInput();
+	CleanupMyDx();
+	CleanupMyWindow();
+
+	return 0;
+
+}
