a28e04a270f802f18deaecad53394f4cdd6aca77
gremoryyx <arispelgremory@gmail.com>
2022-09-28 00:27:19 +0800

    added scenes

---
+++ Scenes.cpp
@@ -0,0 +1,35 @@
+﻿#include "Scenes.h"
+Scenes::Scenes()
+= default;
+
+Scenes::~Scenes()
+= default;
+
+void Scenes::Initialize(LPD3DXSPRITE brush, IDirect3DDevice9* d3dDevice)
+{
+}
+
+void Scenes::GetInput()
+{
+}
+
+void Scenes::Sprite(LPD3DXSPRITE brush)
+{
+}
+
+void Scenes::Font(LPD3DXSPRITE brush, IDirect3DDevice9* d3dDevice)
+{
+}
+
+void Scenes::Line()
+{
+}
+
+void Scenes::Update()
+{
+}
+
+void Scenes::Clean()
+{
+}
+
---
+++ Scenes.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "GameConfiguration.h"
+
+class Scenes {
+    public:
+        Scenes();
+        ~Scenes();
+
+        virtual void Initialize(LPD3DXSPRITE spriteBrush, IDirect3DDevice9* d3dDevice);
+        virtual void GetInput();
+        virtual void Sprite(LPD3DXSPRITE spriteBrush);
+        virtual void Font(LPD3DXSPRITE spriteBrush, IDirect3DDevice9* d3dDevice);
+
+        virtual void Line();
+        virtual void Update();
+        virtual void Clean();
+    
+};
